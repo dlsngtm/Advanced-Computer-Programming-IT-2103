ad942c97113e5e5a7a6da12939293f8319682954
Trisha Marie C. Dalusung <145734104+dlsngtm@users.noreply.github.com>
2023-09-22 11:59:58 +0800

    Add files via upload

---
+++ Act 4.py
@@ -0,0 +1,41 @@
+print("Hello Peeps!")
+
+print("This is from a line of code")
+
+print(5)
+print(5.5)
+print("Hello World!")
+
+num1 = 40
+num2 = 39
+sum = num1 + num2
+print("The sum is:", sum)
+
+a = 7
+product = 5 * a
+print("The product is:", product)
+
+a = 2
+b = 11
+a = a * 2
+c = a * b
+b = b + (c * 3)
+print("c =", c)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print("The sum of the two numbers is:", sum)
+
+squared = 3 ** 2
+cubed = 3 ** 3
+print("Squared:", squared)
+print("Cubed:", cubed)
+
+name = "Marie"
+print("Your name is", name)
+
+Apple_price = 20
+orange_price = 20
+result = Apple_price == orange_price
+print(result)
