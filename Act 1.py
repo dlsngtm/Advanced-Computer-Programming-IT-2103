5b567d7f97c69d1a596e527a1870f087971d042f
Trisha Marie C. Dalusung <145734104+dlsngtm@users.noreply.github.com>
2023-09-22 10:47:10 +0800

    Delete Act 1.py

--- Act 1.py
+++
@@ -1,7 +0,0 @@
-name = input('Enter your Name: ')
-age = int (input ('Enter your Age: '))
-address = input('Enter your Address: ')
-
-print(f"Name = {name}")
-print(f"Age = {age}")
-print(f"Address = {address}")
